b57c177be0ec7a213efa17d38f4d93fabb52ca75
Vdovenkova <ivdovenkova@mail.ru>
2020-11-25 18:50:49 +0300

    кнопка "Больше"

--- src/script.js
+++ src/script.js
@@ -76,3 +76,23 @@ const accordions = () => {
   });
 };
 accordions();
+
+// кнопка Больше
+const showHiddenBlocks = () => {
+// visible-sm-block - этот класс виден на ширине экрана от 992 до 768, на остальных не виден
+// hidden - этот класс скрыт всегда
+// добавила трём скрытым элементам класс hidden-block
+  const hiddenBlock = document.querySelectorAll('.hidden-block'),
+    btnMore = document.querySelector('.add-sentence-btn');
+
+  btnMore.addEventListener('click', () => {
+    hiddenBlock.forEach((elem) => {
+      if (elem.classList.contains('visible-sm-block') || elem.classList.contains('hidden')) {
+        elem.classList.remove('visible-sm-block');
+        elem.classList.remove('hidden');
+      }
+    });
+    btnMore.style.display = 'none';
+  });
+};
+showHiddenBlocks();
