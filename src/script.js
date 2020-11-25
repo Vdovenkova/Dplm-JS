51ec31a02b10e9093021b9f87f7db0fda4e80569
Vdovenkova <ivdovenkova@mail.ru>
2020-11-25 14:32:27 +0300

    сделаны всплывающие окна

--- src/script.js
+++ src/script.js
@@ -1,1 +1,56 @@
 'use strict';
+
+// всплывающие окна
+const modal = () => {
+  const btnPopup = document.querySelectorAll('.btn-popup'), // это кнопки которые вызывают модалки
+        popup = document.querySelectorAll('.popup'); //это мод.окна
+
+  // открытие
+  btnPopup.forEach((elem) => {
+    elem.addEventListener('click', (event) => {
+      let target = event.target;
+      if (target.matches('a.call-btn')) {
+        event.preventDefault();
+        document.querySelector('.popup-call').style.display = 'block';
+      }
+      if (target.classList.contains('discount-btn') || target.classList.contains('construct-btn')){
+        document.querySelector('.popup-discount').style.display = 'block';
+      }
+      if (target.classList.contains('gauging-button')){
+        document.querySelector('.popup-check').style.display = 'block';
+      }
+      if (target.classList.contains('consultation-btn')){
+        event.preventDefault();
+        document.querySelector('.popup-consultation').style.display = 'block';
+      }
+      // if (window.innerWidth > 768) {
+      //   animation();
+      // }
+    });
+  });
+
+  // очистка инпутов
+  const clearInp = (elem) => {
+    elem.querySelectorAll('input').forEach((item) => {
+      item.value = '';
+    });
+  };
+
+  // закрытие модалок
+  popup.forEach((elem) => {
+    elem.addEventListener('click', (event) => {
+    let target = event.target;
+    if (target.classList.contains('popup-close')) {
+      elem.style.display = 'none';
+      clearInp(elem);
+    } else {
+        target = target.closest('.popup-content');
+        if(!target) {
+          elem.style.display = 'none';
+          clearInp(elem);
+        }
+      }
+    });
+  });
+};
+modal();
