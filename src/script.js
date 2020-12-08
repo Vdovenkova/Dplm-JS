87d4b535139772c5fa692753645b1f8ccd5225ef
Vdovenkova <ivdovenkova@mail.ru>
2020-12-08 21:05:05 +0300

    3 модалки - открытие закрытие

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ const getClubsList = () => {
   const clubsList = document.querySelector('.clubs-list>ul');
   clubsList.style.display = 'none';
 
-  const toggleMenu = () => {
+  const toggleClubsList = () => {
     if (clubsList.style.display === 'none') {
       clubsList.style.display = 'block';
     } else if (clubsList.style.display === 'block') {
@@ -15,10 +15,53 @@ const getClubsList = () => {
   document.addEventListener('click', (event) => {
     let target = event.target;
     if (target.matches('.clubs-list>p') || target.matches('.clubs-list')) {
-      toggleMenu();
+      toggleClubsList();
     } else if (!target.matches('.clubs-list>p') || !target.matches('.clubs-list')) {
       clubsList.style.display = 'none';
     }
   });
 };
 getClubsList();
+
+// всплывающие окна
+const modal = () => {
+  // открываем окна
+  document.querySelectorAll('.btn-popup').forEach((elem) => {
+    elem.addEventListener('click', (event) => {
+      event.preventDefault();
+      let target = event.target;
+      let idPopup = target.dataset.popup.substr(1);
+      document.getElementById(idPopup).style.display = 'block';
+      if (target.closest('.fixed-gift')) {
+        target.style.display = 'none';
+      }
+    });
+  });
+
+  // очистка инпутов
+  const clearInp = (elem) => {
+    elem.querySelectorAll('input').forEach((item) => {
+      item.value = '';
+    });
+  };
+
+  // закрываем окна
+  document.querySelectorAll('.popup').forEach((elem) => {
+    elem.addEventListener('click', (event) => {
+    let target = event.target;
+    if (target.classList.contains('close_icon') ||
+      target.classList.contains('close-btn')) {
+      elem.style.display = 'none';
+      clearInp(elem);
+    } else {
+        target = target.closest('.form-content');
+        if(!target) {
+          elem.style.display = 'none';
+          clearInp(elem);
+        }
+      }
+    });
+  });
+
+};
+modal();
