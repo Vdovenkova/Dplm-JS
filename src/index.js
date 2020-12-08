8396cb63d810352c272242c6233deb0f0f4ca955
Vdovenkova <ivdovenkova@mail.ru>
2020-12-08 13:47:47 +0300

    Средний диплом по JS - готово раскрытие списка клубов

---
+++ src/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import popup from './modules/popup';
+import sliders from './modules/sliders';
+// import accordions from './modules/accordions';
+// import showHiddenBlocks from './modules/showHiddenBlocks';
+// import calculator from './modules/calculator';
+import sendForm from './modules/sendForm';
+
+popup();
+sliders();
+// accordions();
+// showHiddenBlocks();
+// calculator();
+sendForm();
---
+++ src/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const getClubsList = () => {
+  const clubsList = document.querySelector('.clubs-list>ul');
+  clubsList.style.display = 'none';
+
+  const toggleMenu = () => {
+    if (clubsList.style.display === 'none') {
+      clubsList.style.display = 'block';
+    } else if (clubsList.style.display === 'block') {
+      clubsList.style.display = 'none';
+    }
+  };
+
+  document.addEventListener('click', (event) => {
+    let target = event.target;
+    if (target.matches('.clubs-list>p') || target.matches('.clubs-list')) {
+      toggleMenu();
+    } else if (!target.matches('.clubs-list>p') || !target.matches('.clubs-list')) {
+      clubsList.style.display = 'none';
+    }
+  });
+};
+getClubsList();
