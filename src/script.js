b86a7ac87b8a1ae35b0092c08714c2c08883d13a
Vdovenkova <ivdovenkova@mail.ru>
2020-12-09 17:50:49 +0300

    сделана отправка форм на главной странице

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// выпадающий список клубов
 const getClubsList = () => {
   const clubsList = document.querySelector('.clubs-list>ul');
   clubsList.style.display = 'none';
@@ -65,3 +66,136 @@ const modal = () => {
 
 };
 modal();
+
+// Отправка данных из форм
+const sendForm = () => {
+  const forms = document.querySelectorAll('form');
+  const successMessage = document.getElementById('thanks'),
+        errorMessage = document.getElementById('error-send');
+
+  // const errorMessage = 'Ошибка! Что-то пошло не так..',
+  //   successMessage = 'Спасибо, мы скоро с Вами свяжемся!';
+  
+  const loadMessage = document.createElement('div');
+    let styleLoadMsg;
+
+  const statusMessage = document.createElement('div');
+  statusMessage.style.cssText = `
+    margin-top: 10px;
+    font-size: 18px;
+    color: #ffd11a;
+    text-align: center`;
+
+  const spinner = (elem) => {
+    loadMessage.classList.add('sk-flow');
+    elem.appendChild(loadMessage);
+    loadMessage.innerHTML = `
+      <div class="sk-flow-dot"></div>
+      <div class="sk-flow-dot"></div>
+      <div class="sk-flow-dot"></div>`;
+
+    styleLoadMsg = document.createElement('style');
+    styleLoadMsg.textContent = `
+        :root {
+          --sk-size: 40px;
+          --sk-color: #ffd11a;
+        }
+
+        .sk-flow {
+          margin: auto;
+          width: calc(var(--sk-size) * 1.3);
+          height: calc(var(--sk-size) * 1.3);
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+
+        .sk-flow-dot {
+          width: 25%;
+          height: 25%;
+          background-color: var(--sk-color);
+          border-radius: 50%;
+          animation: sk-flow 1.4s cubic-bezier(0.455, 0.03, 0.515, 0.955) 0s infinite
+            both;
+        }
+
+        .sk-flow-dot:nth-child(1) {
+          animation-delay: -0.3s;
+        }
+        .sk-flow-dot:nth-child(2) {
+          animation-delay: -0.15s;
+        }
+
+        @keyframes sk-flow {
+          0%,
+          80%,
+          100% {
+            transform: scale(0.3);
+          }
+          40% {
+            transform: scale(1);
+          }
+        }`;
+    document.head.appendChild(styleLoadMsg);
+  };
+
+  const delSpinner = () => {
+    loadMessage.remove();
+    styleLoadMsg.remove();
+  };
+
+  const postData = (body) => {
+    return fetch('./server.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });
+  };
+
+  forms.forEach((elem) => {
+    elem.addEventListener('submit', (event) => {
+      event.preventDefault();
+      spinner(elem);
+      const formData = new FormData(elem);
+      let body = {};
+      formData.forEach((value, key) => {
+        body[key] = value;
+      });
+
+      postData(body)
+        .then((respons) => {
+          if (respons.status !== 200) {
+            throw new Error('Status network not 200');
+          }
+          delSpinner();
+          elem.reset();
+          if (elem.closest('.popup')) {
+            let idForm = elem.getAttribute('name'),
+            formWrap = document.getElementById(idForm);
+            formWrap.style.display = 'none';
+          }
+          successMessage.style.display = 'block';
+        })
+        .catch((error) => {
+          delSpinner();
+          // очищаем форму
+          // убираем модалку
+          // вызываем модалку id="error-send"
+          // вместо двух следующих строк
+          elem.reset();
+          if (elem.closest('.popup')) {
+            let idForm = elem.getAttribute('name'),
+            formWrap = document.getElementById(idForm);
+            formWrap.style.display = 'none';
+          }
+          errorMessage.style.display = 'block';
+          // elem.appendChild(statusMessage);
+          // statusMessage.textContent = errorMessage;
+          console.error(error);
+        });
+    });
+  });
+};
+sendForm();
