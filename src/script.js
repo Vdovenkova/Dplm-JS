d7a07bf1767795e92400301ceeed7bf05c6a0241
Vdovenkova <ivdovenkova@mail.ru>
2020-12-17 17:38:50 +0300

    калькулятор цены клубной карты

--- src/script.js
+++ src/script.js
@@ -256,3 +256,51 @@ validElems('card_order');
 validElems('footer_form');
 validElems('form1');
 validElems('form2');
+
+// Калькулятор
+const calculator = () => {
+  const priceTotal = document.getElementById('price-total'); // окончательная цена
+  const cardSelection = document.getElementById('card_order'); // вся форма калькулятора
+  const promo = document.getElementById('promo'); // промокод
+  const cards = document.querySelectorAll('input[name="card-type"]'); // это все карты
+  const clubs = document.querySelectorAll('input[name="club-name"]'); // это клубы
+
+  // расчёты
+  const countSumm = () => {
+    let total;
+    let cardValue;
+    let clubValue;
+
+    cards.forEach((elem) => {
+      if (elem.checked) {
+        cardValue = elem.value;}
+    });
+
+    clubs.forEach((elem) => {
+      if (elem.checked) {
+        clubValue = elem.value;}
+    });
+
+    if (clubValue === 'mozaika' && cardValue === '1') {total = 1999;}
+    if (clubValue === 'mozaika' && cardValue === '6') {total = 9900;}
+    if (clubValue === 'mozaika' && cardValue === '9') {total = 13900;}
+    if (clubValue === 'mozaika' && cardValue === '12') {total = 19900;}
+    if (clubValue === 'schelkovo' && cardValue === '1') {total = 2999;}
+    if (clubValue === 'schelkovo' && cardValue === '6') {total = 14990;}
+    if (clubValue === 'schelkovo' && cardValue === '9') {total = 21990;}
+    if (clubValue === 'schelkovo' && cardValue === '12') {total = 24990;}
+    
+    if (promo.value === 'ТЕЛО2020') {total = total * 0.7;}
+
+    total = Math.floor(total);
+    priceTotal.textContent = total;
+  };
+
+  cardSelection.addEventListener('input', (event) => {
+    let target = event.target;
+    if(target.matches('input')) {
+      countSumm();}
+  });
+
+};
+calculator();
