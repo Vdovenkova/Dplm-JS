245b4e2a8404ef6cd62e35523b62c235454f9540
Vdovenkova <ivdovenkova@mail.ru>
2021-01-03 19:12:01 +0300

    слайдер-карусель без адаптива и доб.фиксацию полоски меню при уменьшении экрана

--- src/script.js
+++ src/script.js
@@ -552,22 +552,31 @@ const fixMenu = () => {
     }`;
   document.head.append(styleTopMenu);
 
-  // класс из стиля либо добавляестя либо удаляется при скролле
+  const addClasses = () => {
+    topMenu.classList.add('pos-fix');
+    headBlock.classList.add('mg-bttm');
+  };
+  const delClasses = () => {
+    topMenu.classList.remove('pos-fix');
+    headBlock.classList.remove('mg-bttm');
+  };
+
+  // при скролле полоска убирается или появляется путём
+  // добавляения либо удаления класса из стиля
   document.addEventListener('scroll', () => {
     let headHeight = headBlock.offsetHeight;
     if (window.innerWidth < 768 && window.pageYOffset >= headHeight) {
-      topMenu.classList.add('pos-fix');
-      headBlock.classList.add('mg-bttm');
+      addClasses();
     } else if (window.pageYOffset < headHeight) {
-      topMenu.classList.remove('pos-fix');
-      headBlock.classList.remove('mg-bttm');
+      delClasses();
     }
   });
-  // убираем фиксацию меню при изменении ширины экрана
+  // при изменении ширины экрана полоска меню убирается или появляется
   window.addEventListener('resize', () => {
     if (window.innerWidth >= 768 && topMenu.classList.contains('pos-fix')) {
-      topMenu.classList.remove('pos-fix');
-      headBlock.classList.remove('mg-bttm');
+      delClasses();
+    } else if (window.innerWidth < 768) {
+      addClasses();
     }
   });
 };
@@ -593,3 +602,147 @@ const smoothScroll = () => {
   });
 };
 smoothScroll();
+
+// Слайдер-карусель в блоке section id="services"
+class SliderCarousel {
+  constructor ({
+    main,
+    wrap,
+    next,
+    prev,
+    infinity = false,
+    slidesToShow = 5,
+    position = 0,
+    responsive = []
+  }){
+    if (!main || !wrap) {
+      console.warn('Slader-carousel: Неодходимо добавить свойства "main" и "wrap"');
+    }
+    this.main = document.querySelector(main);
+    this.wrap = document.querySelector(wrap);
+    this.slides = document.querySelector(wrap).children;
+    this.next = document.querySelector(next);
+    this.prev = document.querySelector(prev);
+    this.slidesToShow = slidesToShow;
+    this.options = {
+      infinity,
+      position,
+      widthSlide: Math.floor(100 / this.slidesToShow),
+      maxPosition: this.slides.length - this.slidesToShow
+    };
+    this.responsive = responsive;
+  }
+
+  init(){
+    this.addCarouselClass();
+    this.addCarouselStyle();
+    this.controlSlider();
+    // console.log(this.options.widthSlide);
+    // if (this.responsive){
+    //   this.responseInit();
+    // }
+  }
+
+  addCarouselClass(){
+    this.main.classList.add('сarousel-slider');
+    this.wrap.classList.add('carousel-slider__wrap');
+    for (let item of this.slides) {
+      item.classList.add('carousel-slider__item');
+    }
+  }
+
+  addCarouselStyle(){
+    let style = document.getElementById('sliderCarousel-style');
+    if (!style){
+      style = document.createElement('style');
+      style.id = 'sliderCarousel-style';
+    }
+    style.textContent = `
+      .сarousel-slider {
+        overflow: hidden !important
+      }
+      .carousel-slider__wrap {
+        transition: transform 0.5s !important;
+        will-change: transform !important;
+      }
+      .carousel-slider__item {
+        flex: 0 0 ${this.options.widthSlide}% !important;
+        margin: 0 auto !important;
+      }`;
+    document.head.appendChild(style);
+  }
+
+  controlSlider(){
+    this.prev.addEventListener('click', this.prevSlider.bind(this));
+    this.next.addEventListener('click', this.nextSlider.bind(this));
+  }
+
+  prevSlider(){
+    if (this.options.infinity || this.options.position > 0) {
+      --this.options.position;
+      if (this.options.position < 0){
+        this.options.position = this.options.maxPosition;
+      }
+      this.wrap.style.transform = `
+          translateX(${-this.options.position * this.options.widthSlide}%)`;
+    }
+  }
+
+  nextSlider(){
+    if (this.options.infinity || this.options.position < this.options.maxPosition){
+      ++this.options.position;
+      if (this.options.position > this.options.maxPosition) {
+        this.options.position = 0;
+      }
+      this.wrap.style.transform = `
+          translateX(${-this.options.position * this.options.widthSlide}%)`;
+    }
+  }
+  /*
+  responseInit(){
+    const slidesToShowDefault = this.slidesToShow;
+    const allResponse = this.responsive.map(item => item.breakpoint);
+    const maxResponse = Math.max(...allResponse);
+
+    const chekResponse = () => {
+      const widthWindow = document.documentElement.clientWidth;
+      if (widthWindow < maxResponse) {
+        for (let i = 0; i < allResponse.length; i++){
+          if (widthWindow < allResponse[i]) {
+            this.slidesToShow = this.responsive[i].slidesToShow;
+            this.options.widthSlide = Math.floor(100 / this.slidesToShow);
+            this.addStyle();
+          }
+        }
+      } else {
+        this.slidesToShow = slidesToShowDefault;
+        this.options.widthSlide = Math.floor(100 / this.slidesToShow);
+        this.addStyle();
+      }
+    };
+    chekResponse();
+    window.addEventListener('resize', chekResponse);
+  }*/
+}
+const carousel = new SliderCarousel({
+  main: '.services-slider',
+  wrap: '.services-slider__wrap',
+  next: '#carousel-next',
+  prev: '#carousel-prev',
+  slidesToShow: 5,
+  infinity: true
+//   responsive: [{
+//       breakpoint: 1024,
+//       slidesToShow: 3
+//     },
+//     {
+//       breakpoint: 768,
+//       slidesToShow: 2
+//     },
+//     {
+//       breakpoint: 576,
+//       slidesToShow: 1
+//     }
+//   ]
+});
+carousel.init();
